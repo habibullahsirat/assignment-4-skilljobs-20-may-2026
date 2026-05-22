8cbbda2561fb110dd3786fff47aba3572d67b1d7
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-22 14:08:10 +0600

    Files modified.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Kizil } from "../welcome/kizil";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kizil" },
+    { name: "description", content: "E-Commerce Kizil" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Kizil />;
 }
---
+++ app/welcome/kizil.tsx
@@ -0,0 +1,3 @@
+export function Kizil() {
+  return <div>Welcome to Kizil</div>;
+}
