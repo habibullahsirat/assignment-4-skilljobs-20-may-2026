0ac0744babd6f68670c367a061e16b52207a0b5f
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-22 15:10:14 +0600

    Navbar design updated.

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-between gap-[2px]">
+      <div className="flex justify-between gap-[2px] w-[1440px] h-[66px] px-[80px] pt-[18px] pb-[8px] mx-[16px] m-[16px]">
         <div>
           <img src="/KIZIL.png" alt="Logo" />
         </div>
@@ -14,13 +14,35 @@ export default function Navbar() {
           </div>
           <span className="text-[14px] font-[400]">Search for products</span>
         </div>
-        <div>User Account</div>
-        <div>Whishlist</div>
-        <div>Cart</div>
+        <div className="flex">
+          <div>
+            <img src="/icon-useraccount.png" alt="" />
+          </div>
+          <span>Your Account</span>
+        </div>
+        <div className="flex">
+          <div>
+            <img src="icon-heart.png" alt="" />
+          </div>
+          <span>Whishlist</span>
+        </div>
+        <div className="flex">
+          <div>
+            <img src="icon-cart.png" alt="" />
+          </div>
+          <span>Cart</span>
+        </div>
       </div>
-      <div className="flex justify-between gap-[10px]">
+      <div className="flex justify-between gap-[2px] w-[1440px] h-[66px] px-[80px] pt-[18px] pb-[8px] mx-[16px] m-[16px]">
         <div className="flex justify-between gap-[20px]">
-          <div>Category</div>
+          <div className="flex bg-black text-white items-center rounded-[5px] px-[20px] py-[10px]">
+            <div>
+              <img src="/icon-nav-align.png" alt="" />
+            </div>
+            <span className="uppercase text-[14px] font-semibold">
+              shop by category
+            </span>
+          </div>
           <div>Collections</div>
           <div>Flash Deals</div>
           <div>New Arrivals</div>
