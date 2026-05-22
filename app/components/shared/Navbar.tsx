c04e8b8e808e7fe57be7884fad7ba58eac4bb886
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-22 14:52:27 +0600

    Navbar design updated.

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -2,9 +2,18 @@ export default function Navbar() {
   return (
     <>
       <div className="flex justify-between gap-[2px]">
-        <h1>Logo</h1>
-        <h2>Contatct</h2>
-        <div>Search</div>
+        <div>
+          <img src="/KIZIL.png" alt="Logo" />
+        </div>
+        <div>
+          <img src="/Phone-Info.png" alt="Phone info" />
+        </div>
+        <div className="flex items-center py-[16px] px-[8px] w-[623px] h-[37px] bg-[#F3F3F3] rounded-[100px]">
+          <div>
+            <img src="/icon-search.png" alt="" />
+          </div>
+          <span className="text-[14px] font-[400]">Search for products</span>
+        </div>
         <div>User Account</div>
         <div>Whishlist</div>
         <div>Cart</div>
