fc5ebcfb095777c7e3da3c410b25b924b081da50
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-24 12:49:24 +0600

    Banner design updated.

--- app/components/homepage/Hero.tsx
+++ app/components/homepage/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <>
-      <div className="bg-[url(/banner1.jpg)]">
+      <div className="bg-[url(/banner1.jpg)] w-[1280px] h-[458px] rounded-[12px] bg-cover bg-center">
         <h1 className="text-[68px] font-[700]">Redefine Your Everyday Style</h1>
       </div>
     </>
