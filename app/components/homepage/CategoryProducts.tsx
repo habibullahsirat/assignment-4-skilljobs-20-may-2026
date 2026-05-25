7c9b15e3c3a7f276eff5ff17204f216e18e8f03a
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-25 21:48:45 +0600

    Code updated.

---
+++ app/components/homepage/CategoryProducts.tsx
@@ -0,0 +1,30 @@
+const products = [
+  {
+    image: "/cat1.png",
+    category: "Tops Collection",
+  },
+  {
+    image: "/cat2.png",
+    category: "Bottoms Wear",
+  },
+  {
+    image: "/cat3.jpg",
+    category: "Loungewear",
+  },
+  {
+    image: "/cat4.jpg",
+    category: "Scarves",
+  },
+  {
+    image: "/cat5.jpg",
+    category: "Accessories",
+  },
+];
+
+export default function CategoryProducts() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
--- app/welcome/kizil.tsx
+++ app/welcome/kizil.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/homepage/Hero";
 import Services from "../components/homepage/Services";
 import Category from "../components/homepage/Category";
+import CategoryProducts from "../components/homepage/CategoryProducts";
 
 export function Kizil() {
   return (
@@ -8,6 +9,7 @@ export function Kizil() {
       <Hero />
       <Services />
       <Category />
+      <CategoryProducts />
     </>
   );
 }
