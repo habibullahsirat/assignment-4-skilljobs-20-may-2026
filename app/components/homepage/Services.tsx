6df536b1adf7c8c77b065a32264460242989249f
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-25 20:12:26 +0600

    Services component updated.

--- app/components/homepage/Services.tsx
+++ app/components/homepage/Services.tsx
@@ -1,6 +1,6 @@
 export default function Services() {
   return (
-    <div className="flex justify-between gap-[10px] w-[80%] items-center ">
+    <div className="flex justify-between gap-[10px] w-[80%] items-center mx-[10%] my-[32px] px-[8px] py-[16px]">
       <div>
         <div className="flex justify-between gap-[20px]">
           <div>
@@ -11,16 +11,46 @@ export default function Services() {
             <p className="text-[#A9AAAC] text-[14px] font-[400]">
               On order over BDT-2000
             </p>
-          </div>
-          <div className="h-full">
             <div className="border-r-2 border-r-gray-400"></div>
           </div>
         </div>
       </div>
 
-      <div>Delivery</div>
-      <div></div>
-      <div>Quality</div>
+      <div>
+        <div>
+          <div className="flex justify-between gap-[20px]">
+            <div>
+              <img src="/icon-clock.png" alt="" />
+            </div>
+            <div>
+              <h2 className="font-[600] text-[16px]">Next Day Delivery</h2>
+              <p className="text-[#A9AAAC] text-[14px] font-[400]">
+                Free - spend over BDT-20,000
+              </p>
+              <div className="border-r-2 border-r-gray-400"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <div className="flex justify-between gap-[20px]">
+              <div>
+                <img src="/icon-quality.png" alt="" />
+              </div>
+              <div>
+                <h2 className="font-[600] text-[16px]">Quality Guarantee</h2>
+                <p className="text-[#A9AAAC] text-[14px] font-[400]">
+                  We guarantee our proucts
+                </p>
+                <div className="border-r-2 border-r-gray-400"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
