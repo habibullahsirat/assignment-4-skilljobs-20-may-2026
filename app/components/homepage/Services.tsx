f4b3e5af2a0d98acb60413731accb2cbce31202a
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-25 19:24:44 +0600

    Services section updated.

--- app/components/homepage/Services.tsx
+++ app/components/homepage/Services.tsx
@@ -1,3 +1,22 @@
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <div className="flex justify-between gap-[10px]">
+      <div>
+        <div className="flex justify-between gap-6">
+          <div>
+            <img src="/icon-delivery.png" alt="" />
+          </div>
+          <div>
+            <h2>Fast, Free Shipping</h2>
+            <p>On order over BDT-2000</p>
+          </div>
+          <div className="border-r-2 border-r-gray-400"></div>
+        </div>
+      </div>
+
+      <div>Delivery</div>
+      <div></div>
+      <div>Quality</div>
+    </div>
+  );
 }
