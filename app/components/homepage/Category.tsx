2d34890792f17cb851151481a0ad2109312a22cd
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-25 21:26:44 +0600

    Category updated.

---
+++ app/components/homepage/Category.tsx
@@ -0,0 +1,12 @@
+export default function Category() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-[42px] font-bold">Shop By Category</h1>
+        <p className="text-[16px] font-[400]">
+          Explore our curated collection and find your perfect style.
+        </p>
+      </div>
+    </>
+  );
+}
--- app/welcome/kizil.tsx
+++ app/welcome/kizil.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/homepage/Hero";
 import Services from "../components/homepage/Services";
+import Category from "../components/homepage/Category";
 
 export function Kizil() {
   return (
     <>
       <Hero />
       <Services />
+      <Category />
     </>
   );
 }
