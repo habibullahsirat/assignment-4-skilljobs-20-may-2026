b6d37e1a45cc7c4dfc2a204e933e25722b6d5df1
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-25 19:17:50 +0600

    Service Component created.

--- app/welcome/kizil.tsx
+++ app/welcome/kizil.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/homepage/Hero";
+import Services from "../components/homepage/Services";
 
 export function Kizil() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
