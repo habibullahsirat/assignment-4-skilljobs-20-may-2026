f0611d2559a2c000d206f87a148476526fdf49bf
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-31 23:14:38 +0600

    removed unnecessary code.

--- app/components/homepage/CategoryProducts.tsx
+++ app/components/homepage/CategoryProducts.tsx
@@ -1,50 +1,3 @@
-// const products = [
-//   {
-//     image: "/cat1.png",
-//     category: "Tops Collection",
-//   },
-//   {
-//     image: "/cat2.png",
-//     category: "Bottoms Wear",
-//   },
-//   {
-//     image: "/cat3.jpg",
-//     category: "Loungewear",
-//   },
-//   {
-//     image: "/cat4.jpg",
-//     category: "Scarves",
-//   },
-//   {
-//     image: "/cat5.jpg",
-//     category: "Accessories",
-//   },
-// ];
-
-// export default function CategoryProducts() {
-//   return (
-//     <div className="flex flex-nowrap gap-4 overflow-x-auto w-[1280px] h-[387px] items-center mx-[15%]">
-//       {products.map((item, index) => (
-//         <div
-//           key={index}
-//           className="flex flex-col overflow-hidden items-center shrink-0 rounded-[16px]"
-//         >
-//           {/* Fixed image size */}
-//           <div className="w-[302px] h-[354px]">
-//             <img
-//               src={item.image}
-//               alt={item.category}
-//               className="w-full h-full object-cover"
-//             />
-//           </div>
-
-//           <p className="mt-2 text-sm text-center">{item.category}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
 const products = [
   {
     image: "/cat1.png",
@@ -81,7 +34,7 @@ export default function CategoryProducts() {
             <img
               src={item.image}
               alt={item.category}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover rounded-[16px]"
             />
           </div>
 
