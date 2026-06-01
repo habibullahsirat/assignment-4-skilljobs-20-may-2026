e5f6ac99df9b7407e49b213b5c24f7a013de55c4
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-06-01 15:03:18 +0600

    Top pick of the season updated.

---
+++ app/components/homepage/TopPick.tsx
@@ -0,0 +1,47 @@
+const products = [
+  {
+    image: "/top1.jpg",
+    category: "Tops Collection",
+  },
+  {
+    image: "/top2.jpg",
+    category: "Bottoms Wear",
+  },
+  {
+    image: "/top3.jpg",
+    category: "Loungewear",
+  },
+  {
+    image: "/top4.jpg",
+    category: "Scarves",
+  },
+];
+
+export default function TopPick() {
+  return (
+    <>
+      <div className="text-center text-[42px] font-[700] font-bold mt-[50px] mb-[50px]">
+        <h1>Top Pick of the Season</h1>
+      </div>
+      <div className="flex flex-nowrap gap-4 overflow-x-auto w-[1280px] h-[387px] items-center mx-[15%] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        {products.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col overflow-hidden items-center shrink-0 rounded-[16px]"
+          >
+            {/* Fixed image size */}
+            <div className="w-[302px] h-[354px]">
+              <img
+                src={item.image}
+                alt={item.category}
+                className="w-full h-full object-cover rounded-[16px]"
+              />
+            </div>
+
+            <p className="mt-2 text-sm text-center">{item.category}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- app/welcome/kizil.tsx
+++ app/welcome/kizil.tsx
@@ -4,6 +4,7 @@ import Category from "../components/homepage/Category";
 import CategoryProducts from "../components/homepage/CategoryProducts";
 import FlashDeals from "../components/homepage/FlashDeals";
 import ProductsGrid from "../components/homepage/ProductsGrid";
+import TopPick from "../components/homepage/TopPick";
 
 export function Kizil() {
   return (
@@ -14,6 +15,7 @@ export function Kizil() {
       <CategoryProducts />
       <FlashDeals />
       <ProductsGrid />
+      <TopPick />
     </>
   );
 }
