257d338ad029cb15af049c2d3969c24bf7aa5087
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-06-06 17:52:18 +0600

    Design updated.

--- app/components/homepage/TopPick.tsx
+++ app/components/homepage/TopPick.tsx
@@ -20,29 +20,30 @@ const products = [
 export default function TopPick() {
   return (
     <>
-      <div className="text-center text-[42px] font-[700] font-bold mt-[50px] mb-[50px]">
-        <h1 className="font-['Urbanist']">Top Pick of the Season</h1>
+      <div className="text-center mt-[50px] mb-[50px]">
+        <h1 className="text-[42px] font-bold font-['Urbanist']">
+          Top Pick of the Season
+        </h1>
       </div>
-      <div className="flex flex-nowrap gap-4 overflow-x-auto w-[1280px] h-[387px] items-center mx-[15%] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-        {products.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col overflow-hidden items-center shrink-0 rounded-[16px]"
-          >
-            {/* Fixed image size */}
-            <div className="w-[302px] h-[354px]">
-              <img
-                src={item.image}
-                alt={item.category}
-                className="w-full h-full object-cover rounded-[16px]"
-              />
-            </div>
 
-            <p className="mt-2 text-[18px] font-[600] font-semibold text-center">
-              {item.category}
-            </p>
-          </div>
-        ))}
+      <div className="w-full px-4 sm:px-[10%]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {products.map((item, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <div className="w-full h-[354px]">
+                <img
+                  src={item.image}
+                  alt={item.category}
+                  className="w-full h-full object-cover rounded-[16px]"
+                />
+              </div>
+
+              <p className="mt-2 text-[18px] font-semibold text-center">
+                {item.category}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
